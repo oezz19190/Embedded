d0bc7798f60b4132ddf4cc804908cab29e84f666
oezz19190 <142591739+oezz19190@users.noreply.github.com>
2023-08-18 15:35:31 +0300

    find 4 in the list

---
+++ python tasks/task1.py
@@ -0,0 +1,12 @@
+input_string = input('Enter elements of a list separated by space ')
+print("\n")
+user_list = input_string.split()
+# print list
+print('list: ', user_list)
+
+# convert each item to int type
+if '4' in user_list:
+    # convert each item to int type
+   print("exist")
+else:
+   print("not")
